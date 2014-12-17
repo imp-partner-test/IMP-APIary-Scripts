46014f82e50c71a009672cf9116237d4ec23147b
oxana.ryashentseva <oxana.ryashentseva@siemens.com>
2014-12-17 11:19:35 +0100

    added separation between V1 and V2

--- git-clone-all.sh
+++ git-clone-all.sh
@@ -1,3 +1,4 @@
 #!/bin/sh 
+git clone https://github.com/imp-partner-test/IMP-North-Implemented.git
 git clone https://github.com/imp-partner-test/IMP-North-Implemented-V2.git
 git clone https://github.com/imp-partner-test/IMP-North-Implemented-DreddTestable.git
